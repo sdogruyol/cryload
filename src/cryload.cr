2fce9c2582ff4b690ebb1d942253135b4c46b278
Sdogruyol <dogruyolserdar@gmail.com>
2015-07-26 23:08:34 +0300

    Added stats

--- src/cryload.cr
+++ src/cryload.cr
@@ -3,12 +3,13 @@ require "http"
 
 module Cryload
   class LoadGenerator
-    def initialize(@host)
-      @request_count = 0
+    def initialize(@host, @number=1000)
+      @stats = Stats.new
       ch = generate_request_channel
       loop do
+        check
         ch.receive
-        p "Total request made: #{@request_count}"
+        @stats.increase_total_request_count
       end
     end
 
@@ -16,12 +17,23 @@ module Cryload
       channel = Channel(Int32).new
       spawn do
         loop do
+          start_time = Time.now
           HTTP::Client.get @host
-          @request_count+= 1
-          channel.send @request_count
+          end_time = Time.now
+          time_taken_in_ms = (end_time - start_time).to_f * 1000.0
+          @stats.add_to_request_times time_taken_in_ms
+          channel.send 0
         end
       end
       channel
+    end
+
+    def check
+      if @stats.total_request_count == @number
+        p "Average time taken per request: #{@stats.average_request_time} ms"
+        p "Total request made: #{@stats.total_request_count}"
+        exit
+      end
     end
   end
 end
---
+++ src/cryload/stats.cr
@@ -0,0 +1,23 @@
+module Cryload
+  class Stats
+    getter :total_request_count
+
+    def initialize
+      @total_request_count = 0
+      @request_times = [] of Float64
+    end
+
+    def increase_total_request_count()
+      @total_request_count+=1
+    end
+
+    def add_to_request_times(time)
+      @request_times << time
+    end
+
+    def average_request_time
+      total_request_time = @request_times.sum
+      total_request_time / total_request_count
+    end
+  end
+end
