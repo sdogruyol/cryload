421e7ec89d72a779a60b719619d0376baccff7f6
Sdogruyol <dogruyolserdar@gmail.com>
2015-08-11 11:28:25 +0300

    Created a seperate cli class to handle command line stuff

--- src/cryload.cr
+++ src/cryload.cr
@@ -36,36 +36,5 @@ module Cryload
     end
   end
 end
-options = {} of Symbol => String
-options[:requests] = "1000"
-OptionParser.parse(ARGV) do |opts|
-  opts.banner = "Usage: ./cryload [options]"
 
-  opts.on("-s SERVER", "--server SERVER", "Target Server") do |v|
-    options[:server] = v
-  end
-
-  opts.on("-n NUMBERS", "--numbers NUMBERS", "Number of requests to make") do |v|
-    options[:numbers] = v
-  end
-
-  opts.on("-h", "--help", "Print Help") do |v|
-    puts opts
-  end
-
-  if ARGV.empty?
-    puts opts
-  end
-
-end.parse!
-
-if options.has_key?(:server) && options.has_key?(:numbers)
-  puts "Preparing to make it CRY for #{options[:numbers]} requests!".colorize(:green)
-  Cryload::LoadGenerator.new options[:server], options[:numbers].to_i
-elsif options.has_key?(:server)
-  puts "You have to specify '-n' or '--numbers' flag to indicate the number of requests to make".colorize(:red)
-elsif options.has_key?(:numbers)
-  puts "You have to specify '-s' or '--server' flag to indicate the target server".colorize(:red)
-else
-  puts "You have to specify '-n' and '-s' flags, for help use '-h'".colorize(:red)
-end
+Cryload::Cli.new
---
+++ src/cryload/cli.cr
@@ -0,0 +1,58 @@
+# Command Line Interface Handler for Cryload
+module Cryload
+  class Cli
+    getter :is_ready
+
+    def initialize
+      @options = {} of Symbol => String
+      @is_ready = false
+      prepare_op
+      input_valid?
+      if @is_ready
+        Cryload::LoadGenerator.new @options[:server], @options[:numbers].to_i
+      end
+    end
+
+    # Prepares OptionParser
+    private def prepare_op
+      @options[:requests] = "1000"
+      OptionParser.parse(ARGV) do |opts|
+        opts.banner = "Usage: ./cryload [options]"
+
+        opts.on("-s SERVER", "--server SERVER", "Target Server") do |v|
+          @options[:server] = v
+        end
+
+        opts.on("-n NUMBERS", "--numbers NUMBERS", "Number of requests to make") do |v|
+          @options[:numbers] = v
+        end
+
+        opts.on("-h", "--help", "Print Help") do |v|
+          puts opts
+        end
+
+        if ARGV.empty?
+          puts opts
+        end
+      end.parse!
+    end
+
+    # Validate the input from command line
+    private def input_valid?
+      @is_ready = if @options.has_key?(:server) && @options.has_key?(:numbers)
+                    puts "Preparing to make it CRY for #{@options[:numbers]} requests!".colorize(:green)
+                    true
+                  elsif @options.has_key?(:server)
+                    puts "You have to specify '-n' or '--numbers' flag to indicate the number of requests to make".colorize(:red)
+                    false
+                  elsif @options.has_key?(:numbers)
+                    puts "You have to specify '-s' or '--server' flag to indicate the target server".colorize(:red)
+                    false
+                  else
+                    puts "You have to specify '-n' and '-s' flags, for help use '-h'".colorize(:red)
+                    false
+                  end
+    end
+
+  end
+end
