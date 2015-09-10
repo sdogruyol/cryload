106e6a54901c903a00e71f318e56a17f088b24a8
Serdar Dogruyol <dogruyolserdar@gmail.com>
2015-09-10 14:24:02 +0300

    Added specs

--- spec/cryload_spec.cr
+++ spec/cryload_spec.cr
@@ -1,9 +1,11 @@
 require "./spec_helper"
+require "http/client"
+require "http/server"
 
 describe Cryload do
-  # TODO: Write tests
-
-  it "works" do
-    false.should eq(true)
+  it "generates load on server" do
+    server = create_server
+    Cryload::LoadGenerator.new "http://localhost:8081", 10
+    server.close
   end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,12 @@
 require "spec"
 require "../src/cryload"
+
+def create_server
+  server = HTTP::Server.new(8081) do |req|
+    HTTP::Response.ok("text/plain", "OK")
+  end
+  spawn do
+    server.listen
+  end
+  server
+end
