b8d2cd313579fd71411b4c8f91bcbdb4a5e3dede
bararchy <bar.hofesh@safe-t.com>
2017-07-10 11:44:18 +0300

    Support for verify none

--- src/cryload/cli.cr
+++ src/cryload/cli.cr
@@ -2,10 +2,10 @@
 module Cryload
   class Cli
     def initialize
-      @options = {} of Symbol => String
+      @options = Hash(Symbol, (String | Bool)).new
       prepare_op
       if input_valid?
-        Cryload::LoadGenerator.new @options[:server], @options[:numbers].to_i, @options[:tls]
+        Cryload::LoadGenerator.new(@options[:server].to_s, @options[:numbers].as(String).to_i, @options[:tls].as(Bool))
       end
     end
 
@@ -23,7 +23,7 @@ module Cryload
         end
 
         opts.on("-t SSL", "--tls SSL", "Verify remote SSL certificate ? [y/n]") do |v|
-          @options[:tls] = v.downcase == "y" ? true : false
+          @options[:tls] = (v.downcase == "y") ? true : false
         end
 
         opts.on("-h", "--help", "Print Help") do |v|
@@ -34,25 +34,25 @@ module Cryload
           puts opts
         end
       end.parse!
+      unless @options.has_key?(:tls)
+        @options[:tls] = false
+      end
     end
 
     # Validate the input from command line
     private def input_valid?
       if @options.has_key?(:server) && @options.has_key?(:numbers)
         puts "Preparing to make it CRY for #{@options[:numbers]} requests!".colorize(:green)
-        true
+        return true
       elsif @options.has_key?(:server)
         puts "You have to specify '-n' or '--numbers' flag to indicate the number of requests to make".colorize(:red)
-        false
+        return false
       elsif @options.has_key?(:numbers)
         puts "You have to specify '-s' or '--server' flag to indicate the target server".colorize(:red)
-        false
+        return false
       else
         puts "You have to specify '-n' and '-s' flags, for help use '-h'".colorize(:red)
-        false
-      end
-      unless @options.has_key?(:tls)
-        @options[:tls] = false
+        return false
       end
     end
   end
